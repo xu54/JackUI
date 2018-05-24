2cbb27590ec87bbf788741a545fa52d75d179037
xu54 <xu54@xu54demacbook-pro.local>
2018-05-24 16:31:25 +0800

    Initial Commit

---
+++ JackUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JackUI
+//
+//  Created by xu54 on 2018/5/24.
+//  Copyright © 2018年 xu54. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
