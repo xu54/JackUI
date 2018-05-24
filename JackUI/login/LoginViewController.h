85d28232b7888068a6329b5b31a04789845b650d
xu54 <xu54@me.com>
2018-05-24 18:50:08 +0800

    需要增加的方法
    需要增加的方法

--- JackUI/login/LoginViewController.h
+++ JackUI/login/LoginViewController.h
@@ -6,8 +6,13 @@
 //  Copyright © 2018年 xu54. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
 
 @interface LoginViewController : UIViewController
 
+  /**
+  需要添加的接口：
+  -(void)loginWithName:(NSString*)name pwd:(NSString*)pwd;
+  */
 @end
