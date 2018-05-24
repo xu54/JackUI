d4c99921eb2c177619f20a6149448a388336ae89
xu54 <xu54@xu54demacbook-pro.local>
2018-05-24 16:35:10 +0800

    init

--- JackUI/ViewController.m
+++ JackUI/ViewController.m
@@ -19,7 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
+-(void)test
+{
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
